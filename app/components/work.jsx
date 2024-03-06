fb3aadfacc16a1cbf1645bcd3d386b4e92d58327
Blackie01 <onioludamola@gmail.com>
2024-03-06 13:47:32 +0100

    fixing the background color change

--- app/components/work.jsx
+++ app/components/work.jsx
@@ -11,36 +11,6 @@ import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
 gsap.registerPlugin(ScrollTrigger);
 
 const Work = () => {
-  const sectionRef = useRef(null);
-
-  useEffect(() => {
-    const handleScrollBgChange = () => {
-      const sectionPosition = sectionRef.current?.getBoundingClientRect();
-      console.log("Section position:", sectionPosition);
-  
-      if (sectionPosition?.top < window.innerHeight && sectionPosition.bottom > 0) {
-        // Change the background color of the parent container
-        sectionRef.current.parentElement.style.backgroundColor = '#000';
-      } else {
-        // Revert back to default background color
-        sectionRef.current.parentElement.style.backgroundColor = '#E1DFDD';
-      }
-    };
-  
-    ScrollTrigger.create({
-      trigger: sectionRef.current,
-      start: 'top top',
-      end: 'bottom bottom',
-      onEnter: handleScrollBgChange,
-      onLeaveBack: handleScrollBgChange,
-    });
-  
-    // Cleanup function
-    return () => {
-      window.removeEventListener('scroll', handleScrollBgChange);
-    };
-  }, []);
-
   return (
     <section id={styles.overallContainer}>
       <div id={styles.row1}>
@@ -53,26 +23,28 @@ const Work = () => {
           </p>
         </div>
         <div className={styles.overlay} id={styles.aibContainer}>
-          <Image src={AIB} alt=''/>
+          <Image src={AIB} alt="" />
         </div>
       </div>
 
       <div id={styles.row2}>
         <div id={styles.row2Left}>
           <div className={styles.overlay} id={styles.syncSkillsContainer}>
-            <Image src={SyncSkills} alt=''/>
+            <Image src={SyncSkills} alt="" />
           </div>
           <div className={styles.overlay} id={styles.scissorsContainer}>
-            <Image src={Scissors} alt=''/>
+            <Image src={Scissors} alt="" />
           </div>
         </div>
         <div id={styles.row2Right}>
-            <div className={styles.overlay} id={styles.automateContainer}>
-            <Image src={Automate} alt=''/>
-            </div>
-            <div id={styles.needMyServiceContainer}>
-            Since I&apos;m only as good as what I build next, I keep learning new web technologies and tools that will improve the quality of my work, one commit at a time.
-            </div>
+          <div className={styles.overlay} id={styles.automateContainer}>
+            <Image src={Automate} alt="" />
+          </div>
+          <div id={styles.needMyServiceContainer}>
+            Since I&apos;m only as good as what I build next, I keep learning
+            new web technologies and tools that will improve the quality of my
+            work, one commit at a time.
+          </div>
         </div>
       </div>
     </section>
--- app/page.js
+++ app/page.js
@@ -1,72 +1,90 @@
-'use client'
-import React, {useEffect} from "react";
+"use client";
+import React, { useEffect, useState } from "react";
 import Image from "next/image";
 import styles from "./page.module.css";
 import Homepage from "./home/page";
 // import Navigation from "../components/nav"
 import Navigation from "./components/nav";
 // import styles from './home.module.css'
-import Hero from "./components/Hero"
-import Construction from "./components/Construction"
-import About from './components/about'
-import Work from './components/work'
-import Contact from './components/contact'
-import gsap from 'gsap';
-import ScrollTrigger from 'gsap/dist/ScrollTrigger';
+import Hero from "./components/Hero";
+import Construction from "./components/Construction";
+import About from "./components/about";
+import Work from "./components/work";
+import Contact from "./components/contact";
+import gsap from "gsap";
+import ScrollTrigger from "gsap/dist/ScrollTrigger";
 
 gsap.registerPlugin(ScrollTrigger);
 
 export default function Home() {
+  // const [isSectionInView, setIsSectionInView] = useState();
 
-  useEffect(() =>{
-    gsap.fromTo( "#main", {
-      backgroundColor: gsap.getProperty("html", "--light")
-    }, {
-      scrollTrigger: {
-        trigger: ".work",
-        scrub: true,
-        start: 'top 40%',
-        end: 'top top',
-        // () => "+=" + document.querySelector(".work").offsetHeight, 
-        duration: 0.05,
+  // useEffect(() => {
+  //   const contactSection = document.querySelector(".contact");
+  //   console.log('contact section', contactSection)
+  //   const handleScroll = () => {
+  //     setIsSectionInView(contactSection.getBoundingClientRect().top >= 0);
+  //   };
+  //   window.addEventListener("scroll", handleScroll);
+  //   return () => {
+  //     window.removeEventListener("scroll", handleScroll);
+  //   };
+  // }, []);
+
+  useEffect(() => {
+    gsap.fromTo(
+      "#main",
+      {
+        backgroundColor: gsap.getProperty("html", "--light"),
       },
-      backgroundColor: gsap.getProperty("html", "--dark")
-    });
+      {
+        scrollTrigger: {
+          trigger: ".work",
+          scrub: true,
+          start: "top 40%",
+          end: "top top",
+          duration: 0.05,
 
-  }, [])
+          toggleActions: "play reverse play reverse",
+        },
+        backgroundColor: gsap.getProperty("html", "--dark"),
+      }
+    );
 
-  // useEffect(() =>{
-  //   gsap.fromTo( ".contact", {
-  //     backgroundColor: gsap.getProperty("html", "")
-  //   }, {
-  //     scrollTrigger: {
-  //       trigger: ".contact",
-  //       scrub: true,
-  //       start: 'top 40%',
-  //       end: 'top top',
-  //       duration: 0.05,
-  //     },
-  //     backgroundColor: gsap.getProperty("html", "--light")
-  //   });
-
-  // }, [])
- 
-
+    
+      gsap.fromTo(
+        "#main",
+        {
+          // backgroundColor: gsap.getProperty("main", "--dark"),
+        },
+        {
+          scrollTrigger: {
+            trigger: ".contact",
+            scrub: true,
+            start: "top 80%",
+            end: "top top",
+            duration: 0.05,
+          },
+          backgroundColor: gsap.getProperty("html", "--light"),
+        }
+      );
+    
+  }, []);
 
   return (
     <main id="main" className={styles.main}>
       {/* <Homepage /> */}
 
-      <Navigation/>
-            <Hero/>
-            <Construction/>
-            <About/>
-            <div className="work">
-            <Work/>
-            </div>
-            <div className="contact">
-            <Contact/>
-            </div>
+      <Navigation />
+      <Hero />
+      <Construction />
+      <About />
+      <div className="work">
+        <Work />
+      </div>
+      <div className="contact">
+        <Contact />
+      </div>
     </main>
   );
 }
